488bcdbd83313c82e96143ae7023e7b7ffa52afe
root <root@DESKTOP-UE7169K>
2025-02-13 12:43:11 +0900

    chore: 잘못된 경로 수정

--- src/applications/port/in-bound/user.service.port.ts
+++ src/applications/port/in-bound/user.service.port.ts
@@ -1,6 +1,6 @@
 import { RequestSaveUserDto } from 'src/applications/adapter/in-bound/dtos/requests/request-save-user.dto';
 import { RequestUpdateUserDto } from 'src/applications/adapter/in-bound/dtos/requests/request-update-user.dto';
-import { ResponseGetIdealTypeDto } from 'src/applications/adapter/in-bound/dtos/responses/response-get-ideal-type.dto';
+import { ResponseGetIdealTypeDto } from 'src/applications/use-case/dtos/responses/response-get-ideal-type.dto';
 import { ResponseGetUserIdTokenDto } from 'src/applications/use-case/dtos/responses/response-get-user-id-token.dto';
 import { ResponseGetUserIdDto } from 'src/applications/use-case/dtos/responses/response-get-user-id.dto';
 import { ResponseGetUserPhoneNumberDto } from 'src/applications/use-case/dtos/responses/response-get-user-phone-number.dto';
